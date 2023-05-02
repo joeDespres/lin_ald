87da9b1ab63361bd39c5750649948ace24bc9954
joeDespres <joe.d@gastrograph.com>
2023-05-01 20:02:40 -0400

    move methods to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use ndarray::prelude::*;
 use std::{process::Output, time::Instant};
 
+mod metd;
+use crate::metd::FloatMethods;
 mod vec;
 
 fn main() {
@@ -13,81 +15,4 @@ fn main() {
     dbg!(kernel);
 
     dbg!(start_time.elapsed());
-}
-
-trait MyVecMethods<T>
-where
-    T: ndarray::LinalgScalar,
-{
-    fn kernel_mul(&self, kernel: &Array1<T>) -> Array1<T>;
-}
-
-impl<T> MyVecMethods<T> for Array1<T>
-where
-    T: ndarray::LinalgScalar,
-{
-    fn kernel_mul(&self, kernel: &Array1<T>) -> Array1<T> {
-        let l = kernel.len();
-
-        self.windows(l)
-            .into_iter()
-            .map(|x| x.dot(kernel))
-            .collect::<Vec<T>>()
-            .into()
-    }
-}
-
-trait FloatMethods<T>
-where
-    T: ndarray::NdFloat,
-{
-    fn l2_norm(&self) -> T;
-    fn unit_vec(&self) -> Array1<T>;
-    fn set_magnitude(&self, l: T) -> Array1<T>;
-}
-
-impl<T> FloatMethods<T> for Array1<T>
-where
-    T: ndarray::NdFloat,
-{
-    fn l2_norm(&self) -> T {
-        self.dot(self).sqrt()
-    }
-    fn unit_vec(&self) -> Array1<T> {
-        self / self.l2_norm()
-    }
-    fn set_magnitude(&self, l: T) -> Array1<T> {
-        self.unit_vec() * l
-    }
-}
-
-#[test]
-fn testing_l2_norm() {
-    use vec::TOL;
-    let norm = arr1(&[1., 2., 2.]).l2_norm();
-    approx::assert_relative_eq!(norm, 3., epsilon = TOL);
-}
-#[test]
-fn test_unit_vec() {
-    use vec::TOL;
-    let x = arr1(&[3.0, 4.0, 3.0]);
-    let expected = arr1(&[
-        0.514_495_755_427_526_5,
-        0.685_994_340_570_035_3,
-        0.514_495_755_427_526_5,
-    ]);
-    let result = x.unit_vec();
-    assert_eq!(result, expected);
-    for _ in 1..1_000 {
-        let v = vec::normal_vec(69);
-        let unit_vec_v = v.unit_vec();
-        let length = unit_vec_v.mapv(|x| x.powi(2)).sum();
-        approx::assert_relative_eq!(length, 1.0, epsilon = TOL);
-    }
-}
-#[test]
-fn test_set_vec_len() {
-    use vec::TOL;
-    let len_v = vec::normal_vec(69).set_magnitude(9.0).l2_norm();
-    approx::assert_relative_eq!(len_v, 9.0, epsilon = TOL);
 }
