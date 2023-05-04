d80680c5bc5394030e2fb23a9767369fd257a6f7
joeDespres <joe.d@gastrograph.com>
2023-05-03 20:31:55 -0400

    add matrix methods

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use approx::assert_relative_eq;
 use ndarray::prelude::*;
 use std::time::Instant;
 
@@ -8,45 +7,9 @@ mod vec;
 
 fn main() {
     let start_time = Instant::now();
-
-    let d = 2;
+    let d = 5;
     let a = Array::from_shape_vec((d, d), gen_brownian_motion(d * d).to_vec()).unwrap();
     let b = Array::from_shape_vec((d, d), gen_brownian_motion(d * d).to_vec()).unwrap();
-
-    show_communitivity(a.clone(), b.clone(), 0.2);
-    dbg!(a.max());
-
     dbg!(start_time.elapsed());
-}
-
-fn show_communitivity(a: Array2<f64>, b: Array2<f64>, sigma: f64) {
-    let r1 = sigma * (a.clone() + b.clone());
-    let r2 = sigma * a.clone() + sigma * b.clone();
-    let r3 = a.clone() * sigma + b.clone() * sigma;
-    let diff1 = r1.clone() - r2.clone();
-    let diff2 = r1.clone() - r3.clone();
-    let diff3 = r2.clone() - r3.clone();
-    // assert!(diff1.max().unwrap() < vec::TOL);
-    // assert!(diff2.max().unwrap() < vec::TOL);
-    // assert!(diff3.max().unwrap() < vec::TOL);
-}
-
-pub trait MyMatrixMethods<T>
-where
-    T: ndarray::NdFloat,
-{
-    fn max(&self) -> f64;
-}
-
-impl<T> MyMatrixMethods<T> for Array2<T>
-where
-    T: ndarray::NdFloat,
-{
-    fn max(&self) -> f64 {
-        dbg!(self.clone().into_raw_vec());
-        let vec = self.clone().into_raw_vec().sort_unstable();
-        dbg!(vec);
-
-        return 69.0;
-    }
+    dbg!(a, b);
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,4 @@
+use crate::vec;
 use ndarray::{concatenate, prelude::*};
 
 #[allow(dead_code)]
@@ -40,4 +41,51 @@ fn test_element_addition() {
     let target = Array::from_shape_vec((1_000, 1_000), a.to_vec()).unwrap();
 
     assert_eq!(c, target);
+}
+#[test]
+fn test_communitivity() {
+    for i in 1..100 {
+        let d = i;
+        let a = Array::from_shape_vec((d, d), vec::gen_brownian_motion(d * d).to_vec()).unwrap();
+        let b = Array::from_shape_vec((d, d), vec::gen_brownian_motion(d * d).to_vec()).unwrap();
+        show_communitivity(a.clone(), b.clone(), 0.2);
+    }
+}
+
+#[allow(dead_code)]
+fn show_communitivity(a: Array2<f64>, b: Array2<f64>, sigma: f64) {
+    let r1 = sigma * (a.clone() + b.clone());
+    let r2 = sigma * a.clone() + sigma * b.clone();
+    let r3 = a.clone() * sigma + b.clone() * sigma;
+    let diff1 = r1.clone() - r2.clone();
+    let diff2 = r1.clone() - r3.clone();
+    let diff3 = r2.clone() - r3.clone();
+
+    assert!(diff1.max() < vec::TOL);
+    assert!(diff2.max() < vec::TOL);
+    assert!(diff3.max() < vec::TOL);
+}
+
+pub trait MyMatrixMethods<T>
+where
+    T: ndarray::NdFloat,
+{
+    fn max(&self) -> T;
+    fn min(&self) -> T;
+}
+
+impl<T> MyMatrixMethods<T> for Array2<T>
+where
+    T: ndarray::NdFloat,
+{
+    fn max(&self) -> T {
+        let mut vec = self.clone().into_raw_vec();
+        vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        *vec.first().unwrap()
+    }
+    fn min(&self) -> T {
+        let mut vec = self.clone().into_raw_vec();
+        vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        *vec.last().unwrap()
+    }
 }
