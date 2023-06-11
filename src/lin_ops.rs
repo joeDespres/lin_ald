b91cc6f1b624c00a491c82022bc99d9fe3ec8f6e
despresj <54477023+despresj@users.noreply.github.com>
2023-06-11 09:19:54 -0400

    add matrix row removal

--- src/lin_ops.rs
+++ src/lin_ops.rs
@@ -1,5 +1,7 @@
+use ndarray::Array;
 use ndarray::Array2;
-use ndarray_linalg::Determinant;
+use ndarray::Axis;
+// use ndarray_linalg::Determinant;
 
 pub trait MyLinOps<T>
 where
@@ -18,6 +20,50 @@ where
         dbg!(n_rows);
         dbg!(n_cols);
 
+        dbg!(&self);
+        let mut iter = 0;
+        for row_i in 0..n_rows {
+            for col_j in 0..n_cols {
+                iter += 1;
+                let a = remove_rows(self, &[row_i]);
+                let b = remove_cols(&a, &[col_j]);
+                dbg!(b);
+
+                // dbg!(sliced_mat);
+            }
+        }
+
         self.clone()
     }
+}
+
+fn remove_rows<A: Clone>(matrix: &Array2<A>, to_remove: &[usize]) -> Array2<A> {
+    let mut keep_row = vec![true; matrix.nrows()];
+    to_remove.iter().for_each(|row| keep_row[*row] = false);
+
+    let elements_iter = matrix
+        .axis_iter(Axis(0))
+        .zip(keep_row.iter())
+        .filter(|(_row, keep)| **keep)
+        .flat_map(|(row, _keep)| row.to_vec());
+
+    let new_n_rows = matrix.nrows() - to_remove.len();
+    Array::from_iter(elements_iter)
+        .into_shape((new_n_rows, matrix.ncols()))
+        .unwrap()
+}
+fn remove_cols<A: Clone>(matrix: &Array2<A>, to_remove: &[usize]) -> Array2<A> {
+    let mut keep_col = vec![true; matrix.ncols()];
+    to_remove.iter().for_each(|row| keep_col[*row] = false);
+
+    let elements_iter = matrix
+        .axis_iter(Axis(1))
+        .zip(keep_col.iter())
+        .filter(|(_col, keep)| **keep)
+        .flat_map(|(col, _keep)| col.to_vec());
+
+    let new_n_cols = matrix.ncols() - to_remove.len();
+    Array::from_iter(elements_iter)
+        .into_shape((matrix.nrows(), new_n_cols))
+        .unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use ndarray_linalg::Determinant;
+use crate::lin_ops::MyLinOps;
+use ndarray::arr2;
 use std::time::Instant;
-
+mod lin_ops;
 mod matrix;
 mod vec;
 
 fn main() {
     let start_time = Instant::now();
-    let rows = 100;
-    let cols = 100;
 
-    let a = matrix::bm_mat_rec(rows, cols);
-    let det_a = a.det().unwrap();
-    dbg!(det_a);
+    let a = arr2(&[[2.0, 1.0, 2.0], [-2.0, 2.0, 1.0], [1.0, 2.0, -2.0]]);
+
+    dbg!(a.minors_mat());
+
+    // let det_a = a.det().unwrap();
     dbg!(start_time.elapsed());
 }
